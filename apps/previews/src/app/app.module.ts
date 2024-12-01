3ffa29821aafe8d7593de3a9ecb9ff0d3d86c46f
arligle <0774543002@163.com>
2024-12-01 23:51:43 +0800

    feat(packages): created @aiofc/config

--- apps/previews/src/app/app.module.ts
+++ apps/previews/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typedConfigModuleForRoot } from '@aiofc/config';
+import rootConfig from '../config/root.config';
 
 @Module({
-  imports: [],
+  imports: [
+    typedConfigModuleForRoot(__dirname, rootConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/previews/src/main.ts
+++ apps/previews/src/main.ts
@@ -1,18 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
+import { AppConfig } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+   const config = app.get(AppConfig);
+  const globalPrefix = config.prefix || 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
