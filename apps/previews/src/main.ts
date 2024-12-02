c8e22613dd7f7067331df682e91c7041e82aa764
arligle <0774543002@163.com>
2024-12-02 13:19:15 +0800

    feat(apps): previews updata

--- apps/previews/src/main.ts
+++ apps/previews/src/main.ts
@@ -1,32 +1,31 @@
+import { fastifyBootstrap } from "@aiofc/fastify-server";
+import { AppModule } from "./app/app.module";
 
-import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app/app.module';
-import { AppConfig } from './config/app.config';
-import { Logger } from '@aiofc/logger';
+fastifyBootstrap(AppModule);
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  const config = app.get(AppConfig);
-  const logger = app.get(Logger);
+// import { NestFactory } from '@nestjs/core';
+// import {
+//   FastifyAdapter,
+//   NestFastifyApplication,
+// } from '@nestjs/platform-fastify';
+// import { AppModule } from './app/app.module';
+// import { AppConfig } from './config/app.config';
+// import { Logger } from '@aiofc/logger';
 
-   // 在应用程序时注册全局的日志记录器
-  app.useLogger(logger);
-  app.flushLogs(); // 刷新日志：将内存中的日志数据写入到持久存储（如文件或数据库）中
-
-  const globalPrefix = config.prefix || 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = config.port || 3000;
-  await app.listen(port);
-  logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
-
-bootstrap();
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestFastifyApplication>(
+//     AppModule,
+//     new FastifyAdapter()
+//   );
+//   const config = app.get(AppConfig);
+//   const logger = app.get(Logger);
+//   const globalPrefix = config.prefix || 'api';
+//   const port = config.port || 3000;
+//   app.setGlobalPrefix(globalPrefix);
+//   await app.listen(port, () => {
+//     logger.log(
+//       `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+//     );
+//   });
+// }
+// bootstrap();
